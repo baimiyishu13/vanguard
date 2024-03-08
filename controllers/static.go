c4e23917a37d21e598828085bb409bf0c893bdfb
baimiyishu13 <baimiyishu13@163.com>
2024-03-09 00:23:19 +0800

    controllers (static and template)

---
+++ controllers/static.go
@@ -0,0 +1,10 @@
+package controllers
+
+import "net/http"
+
+// StaticHandler 闭包，调用模板的 Execute 方法来将模板呈现给响应
+func StaticHandler(tpl Template) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, nil)
+	}
+}
