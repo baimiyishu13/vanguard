92ff4a3dbc50f1c27be45a64862a9db87bce1396
baimiyishu13 <baimiyishu13@163.com>
2024-03-09 01:32:02 +0800

    gohtml_template

--- controllers/static.go
+++ controllers/static.go
@@ -1,10 +1,13 @@
 package controllers
 
-import "net/http"
+import (
+	"net/http"
+)
 
 // StaticHandler 闭包，调用模板的 Execute 方法来将模板呈现给响应
 func StaticHandler(tpl Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tpl.Execute(w, nil)
 	}
+
 }
--- main.go
+++ main.go
@@ -1,13 +1,32 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/baimiyishu13/vanguard/controllers"
+	"github.com/baimiyishu13/vanguard/templates"
+	"github.com/baimiyishu13/vanguard/views"
+	"github.com/go-chi/chi/v5"
+	"log"
+	"net/http"
+)
 
 func main() {
-	r := gin.New()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello World",
-		})
-	})
-	r.Run(":3003")
+	// test
+	r := chi.NewRouter()
+
+	// sign in 登陆界面
+	r.Get("/signin", controllers.StaticHandler(views.Must(
+		views.ParseFS(templates.FS, "signin.gohtml"))))
+
+	r.Get("/", controllers.StaticHandler(
+		views.Must(views.ParseFS(templates.FS,
+			"signin.gohtml",
+		))))
+
+	fmt.Println("🚀 启动服务器端口:5001 ...")
+	err := http.ListenAndServe(":5001", nil)
+	if err != nil {
+		log.Println("Error listening on :5001")
+		return
+	}
 }
